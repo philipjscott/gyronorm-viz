d07b7b47683ba511fd9031531db96ed624acb5ed
Philip Scott <pscott@zeptohost.com>
2018-01-21 03:03:27 -0800

    Add cube visualization
    * Fulltilt is a dependency of gyronorm, and has a non-commercial license
    * Gyronorm isn't functional on NPM, need to have local file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import * as THREE from 'three'
-import * as GyroNorm from 'gyronorm'
+import { GyroNorm } from '../vendor/gyronorm.complete.min'
 
-
-console.log('branch works')
-const toRadians = angle => angle * (Math.PI / 180)
+const toRadian = angle => angle * (Math.PI / 180)
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
@@ -13,14 +11,16 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xffff00 })
+const material = new THREE.MeshNormalMaterial({ color: 0xffff00 })
 const cube = new THREE.Mesh(geometry, material)
 
 scene.add(cube)
 camera.position.z = 5
+  cube.rotation.y = 1
 
 function animate() {
   requestAnimationFrame(animate)
+
   renderer.render(scene, camera)
 }
 
